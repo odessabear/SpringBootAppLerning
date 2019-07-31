b07717443459e4cc7774a966f9952298d840b952
odessabear@gmail.com <odessabear@gmail.com>
2019-07-31 09:49:15 +0300

    added if user exists and spring security

--- src/main/java/com/amikhailov/mobileappws/service/impl/UserServiceImpl.java
+++ src/main/java/com/amikhailov/mobileappws/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.amikhailov.mobileappws.UserRepository;
 import com.amikhailov.mobileappws.io.entity.UserEntity;
 import com.amikhailov.mobileappws.service.UserService;
 import com.amikhailov.mobileappws.shared.dto.UserDto;
+import com.amikhailov.mobileappws.shared.dto.Utils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,12 +15,20 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
+
+
+
+        if ( userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
-        userEntity.setEncryptedPassword("test");
+        String publicUserId = utils.generateUserId(30);
+        userEntity.setEncryptedPassword(publicUserId);
         userEntity.setUserId("testUserId");
 
 
