bd85728f80a2ac0b57d57eb64261ba1c3bf245c4
odessabear@gmail.com <odessabear@gmail.com>
2019-07-31 19:28:14 +0300

    SecurityConstants class and AuthenticationFilter were implemented

--- src/main/java/com/amikhailov/mobileappws/security/WebSecurity.java
+++ src/main/java/com/amikhailov/mobileappws/security/WebSecurity.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import sun.security.util.SecurityConstants;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -24,7 +23,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "/users").permitAll().anyRequest().authenticated();
+        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
+                .anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
     }
 
     @Override
