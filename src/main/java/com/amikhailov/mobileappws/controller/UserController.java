cea385e7650ff412412245600a8ee4e60ac555bc
odessabear@gmail.com <odessabear@gmail.com>
2019-08-14 09:04:37 +0300

    Adding XML Support & JSON Support completed

--- src/main/java/com/amikhailov/mobileappws/controller/UserController.java
+++ src/main/java/com/amikhailov/mobileappws/controller/UserController.java
@@ -4,9 +4,12 @@ import com.amikhailov.mobileappws.service.UserService;
 import com.amikhailov.mobileappws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import com.amikhailov.mobileappws.model.request.UserDetailsRequestModel;
 import com.amikhailov.mobileappws.model.response.UserRest;
+
+import javax.print.attribute.standard.Media;
 
 @RestController
 @RequestMapping("/users") //http://localhost:8080/users
@@ -15,35 +18,36 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping(path = "/{id}")
-    public UserRest getUser(@PathVariable String id){
+    @GetMapping(path = "/{id}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public UserRest getUser(@PathVariable String id) {
         UserRest returnValue = new UserRest();
 
         UserDto userDto = userService.getUserByUserId(id);
-        BeanUtils.copyProperties(userDto,returnValue);
+        BeanUtils.copyProperties(userDto, returnValue);
         return returnValue;
     }
 
-    @PostMapping
-    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails){
+    @PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
 
         UserRest returnValue = new UserRest();
 
         UserDto userDto = new UserDto();
-        BeanUtils.copyProperties(userDetails,userDto);
+        BeanUtils.copyProperties(userDetails, userDto);
 
         UserDto createUser = userService.createUser(userDto);
-        BeanUtils.copyProperties(createUser,returnValue);
+        BeanUtils.copyProperties(createUser, returnValue);
         return returnValue;
     }
 
     @PutMapping
-    public String updateUser(){
+    public String updateUser() {
         return "update user was called";
     }
 
     @DeleteMapping
-    public String deleteUser(){
+    public String deleteUser() {
         return "delete user was called";
     }
 }
